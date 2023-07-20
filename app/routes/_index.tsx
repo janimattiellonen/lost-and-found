968f15ef669374b23684a2075b99b5b50ab2a7e5
janimattiellonen <janimatti.ellonen@gmail.com>
2023-07-20 21:48:10 +0300

    Lost and found stuff test

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,15 +1,13 @@
 import type { V2_MetaFunction } from "@remix-run/node";
 
 export const meta: V2_MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+  return [{ title: "New Remix App" }];
 };
 
 export default function Index() {
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
