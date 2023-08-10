cdfc61638815a58f71f3fca240b79f0817120490
janimattiellonen <janimatti.ellonen@gmail.com>
2023-08-10 21:10:49 +0300

    Added TT logo

--- app/routes/Header.tsx
+++ app/routes/Header.tsx
@@ -1,7 +1,34 @@
+import styled from "@emotion/styled";
+
 
 type HeaderProps = {
+  clubId: number
   clubName: string
 }
-export default function Header({clubName}: HeaderProps): JSX.Element {
-  return <h1 className="font-bold text-4xl">Löytökiekot - {clubName}</h1>
+
+const Logo = styled.img`
+  width: 50px;
+
+  @media (min-width: 600px) {
+    width: 100px;
+  }
+`
+
+const H1 = styled.h1`
+  font-size: 1rem;
+
+  @media (min-width: 600px) {
+    font-size: 2.25rem;
+  }
+`;
+
+function getClubLogo(clubId: number ): string | undefined {
+  if (clubId === 2) {
+    return '/tt-sini-logo.jpg'
+  }
+
+  return undefined;
+}
+export default function Header({clubId, clubName}: HeaderProps): JSX.Element {
+  return <div className="flex items-center"><Logo className="mr-4" src={getClubLogo(clubId)} alt={""}/><H1 className="font-bold">Löytökiekot - {clubName}</H1></div>
 }
