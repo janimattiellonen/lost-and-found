689ae3b3ab443d57ad92508bf2b6cd1aa9389f17
janimattiellonen <janimatti.ellonen@gmail.com>
2023-08-20 17:27:35 +0300

    Fixes

--- app/models/discs.server.ts
+++ app/models/discs.server.ts
@@ -1,9 +1,9 @@
-import { createConnection } from "~/models/utils";
-import * as process from "process";
+import { createConnection } from '~/models/utils';
+import * as process from 'process';
 
-import { DiscDTO } from "~/types";
+import { DiscDTO } from '~/types';
 
-import { toDTO } from "~/models/DiscMapper";
+import { toDTO } from '~/models/DiscMapper';
 
 export async function getDiscs(): Promise<DiscDTO[]> {
   const clubId = process.env.APP_CLUB_ID;
@@ -11,19 +11,19 @@ export async function getDiscs(): Promise<DiscDTO[]> {
   const supabase = createConnection();
 
   let { data, error } = await supabase
-    .from("discs")
-    .select(
-      "disc_name, disc_colour, disc_manufacturer, owner_name, owner_phone_number, owner_club_name, added_at"
-    )
-    .order("disc_name", { ascending: true })
-    .eq("is_returned_to_owner", false)
-    .eq("can_be_sold_or_donated", false)
-    .eq("club_id", clubId);
+    .from('discs')
+    .select('disc_name, disc_colour, disc_manufacturer, owner_name, owner_phone_number, owner_club_name, added_at')
+    .order('disc_name', { ascending: true })
+    .eq('is_returned_to_owner', false)
+    .eq('can_be_sold_or_donated', false)
+    .eq('club_id', clubId);
 
-  return data ? data.map((row: any) => {
-    if (row["owner_phone_number"]) {
-      row["owner_phone_number"] = row["owner_phone_number"].slice(-4);
-    }
-    return toDTO(row);
-  }) : [];
+  return data
+    ? data.map((row: any) => {
+        if (row['owner_phone_number']) {
+          row['owner_phone_number'] = row['owner_phone_number'].slice(-4);
+        }
+        return toDTO(row);
+      })
+    : [];
 }
